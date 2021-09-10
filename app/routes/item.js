0b7e0e29450c78d985987f534f044bc626d7911d
jabawack <tk.hfes@gmail.com>
2021-09-11 01:23:00 +0900

    ember g service shopping-cart

--- app/routes/item.js
+++ app/routes/item.js
@@ -12,6 +12,6 @@ export default class ItemRoute extends Route {
 
   setupController(controller, model) {
     super.setupController(controller, model);
-    controller.color = model.colors[0].color;
+    // controller.color = model.colors[0].color;
   }
 }
